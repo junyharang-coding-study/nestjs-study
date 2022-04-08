83469ef2784b75efcd9a2524a5472bd9b1c6eacd
junyharang <junyharang8592@gmail.com>
2022-04-09 01:26:28 +0900

    [Board] 게시글 수정 완료

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { title } from 'process';
-import { Board } from './boards.model';
+import { Board, BoardStatus } from './boards.model';
 import { BoardsService } from './boards.service';
 import { CreateBoardDTO } from './dto/create-board.dto';
 
@@ -13,11 +13,6 @@ export class BoardsController {
     // 생성자에 접근 제한자(private)을 선언하면 생략 가능.
         // this.boardsService = boardsService; 
 
-    @Get() 
-    getAllBoard(): Board[] {   // 목록 조회 | :Boards[] = return Type
-        return this.boardsService.getAllBoards();
-    }   // getAllBoard() 끝
-
     @Post()
     createBoard( 
         @Body() createBoardDTO: CreateBoardDTO): Board {   // 게시글 등록
@@ -26,12 +21,26 @@ export class BoardsController {
 
     }   // createBoard() 끝
 
+    @Get() 
+    getAllBoard(): Board[] {   // 목록 조회 | :Boards[] = return Type
+        return this.boardsService.getAllBoards();
+    }   // getAllBoard() 끝
+
     @Get(':id')
     getBoardById(@Param('id') id : string): Board {
 
         return this.boardsService.getBoardById(id);
 
     } // getBoardById(@Param('id') 끝
+
+
+    @Patch(':id/status')
+    updateBoardStatus(@Param('id') id : string, @Body('status') status : BoardStatus) {
+
+        return this.boardsService.updateBoardStatus(id, status);
+
+    }   // updateBoardStatus(@Param('id') id : string, @Body('status') status : BoardStatus) 끝
+
 
     @Delete(':id')
     deleteBoard(@Param('id') id : string): void {
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -8,12 +8,6 @@ import { CreateBoardDTO } from './dto/create-board.dto';
 export class BoardsService {
     private boards: Board[] = [];    // In Memory 저장소(DB)를 이용하기 위한 배열 선언
 
-    getAllBoards(): Board[] {    // 목록 조회 :Boards[] = return Type
-        
-        return this.boards;
-
-    }   // getAllBoards() 끝
-
     createBoards(createBoardDTO : CreateBoardDTO) { // 게시글 등록
 
         const { title, description } = createBoardDTO;
@@ -33,11 +27,31 @@ export class BoardsService {
         return board;
     }   // createBoards(title: string, descriptions: string) 끝
 
+
+    getAllBoards(): Board[] {    // 목록 조회 :Boards[] = return Type
+        
+        return this.boards;
+
+    }   // getAllBoards() 끝
+
+
     getBoardById(id : string): Board {
 
         return this.boards.find((board) => board.id === id);
 
     }   // getBoardById(id : string) 끝
+
+
+    updateBoardStatus(id : string, status : BoardStatus): Board {
+
+        const board = this.getBoardById(id);    // 수정하고자 하는 게시글을 찾아 온다.
+
+        board.status = status;                  // 비밀글 여부 수정
+
+        return board;
+
+    }   // updateBoardStatus(id : string, status : BoardStatus) 끝
+
 
     deleteBoard(id : string): void {
 

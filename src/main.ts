d085f624dd2a77c754e1a360563b75b9ae8aa86f
junyharang <junyharang8592@gmail.com>
2022-04-09 14:22:34 +0900

    설정 파일 구성 완료

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
-  const port = 8080;
   const app = await NestFactory.create(AppModule);
+
+  const serverConfig = config.get('server');
+
+  const port = serverConfig.port;
+
   await app.listen(port);
   Logger.log(
     `Node.js 기반 Nese.js의 Back-End Server가 ${port}번으로 실행 되었습니다!`,
